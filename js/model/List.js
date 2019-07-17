266d6550494456efbf811df41d0a0153337be03a
Julien Wickramatunga <julian_kth@yahoo.fr>
2019-07-17 21:44:30 +0200

    App Models init

---
+++ js/model/List.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function List (boardId, name){
+    this.boardId = boardId;
+    this.name = name;
+}
+
+module.exports = List;
---
+++ js/model/Task.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function Task (listId, description){
+    this.listId = boardId;
+    this.description = description;
+}
+
+module.exports = Task;
